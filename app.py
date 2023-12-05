80e53d1140e212ff298fbd28494b011ae156a393
Mijael Franklin Palacios Leon <mpalacios@grupomuya.local>
2023-12-04 23:14:27 -0500

    se actualiza app.py

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
-import streamlit as st
-
-st.header('Lanzar una moneda')
-
-st.write('Esta aplicación aún no es funcional. En construcción.')
+        import pandas as pd
+        import plotly.express as px
+        import streamlit as st
+        
+        car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+        hist_button = st.button('Construir histograma') # crear un botón
+        
+        if hist_button: # al hacer clic en el botón
+            # escribir un mensaje
+            st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+            # crear un histograma
+            fig = px.histogram(car_data, x="odometer")
+        
+            # mostrar un gráfico Plotly interactivo
+            st.plotly_chart(fig, use_container_width=True)
