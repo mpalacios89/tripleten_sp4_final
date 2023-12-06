edb25e44543fa3d280ac4094174fa117fe3e8b73
Mijael Franklin Palacios Leon <mpalacios@grupomuya.local>
2023-12-05 19:16:24 -0500

    Actualizacion de ruta

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
         
-car_data = pd.read_csv('mpalacios89/tripleten_sp4_final/vehicles_us.csv') # leer los datos
+car_data = pd.read_csv('https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/Data_sprint_4_Refactored/vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
         
 if hist_button: # al hacer clic en el botón
